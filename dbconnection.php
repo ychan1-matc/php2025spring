7fd42ac7f28439c1a6e8a9d309689832b90cc609
student <ychan1@madisoncollege.edu>
2025-05-05 18:39:46 -0500

    dbconnection

---
+++ dbconnection.php
@@ -0,0 +1,8 @@
+<?php
+    /* dbconnection.php */
+
+    // Database connection constants
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'student');
+    define('DB_PASSWORD', 'student');
+    define('DB_NAME', 'diarylisting');
